1fcc895304a9c031ee8c97e269c4a52edbf19bb2
lingling <lingling@italki.com>
2021-04-16 17:50:28 +0800

    add login

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,31 @@
 import React,{useState, useEffect} from "react";
 import TextField from "@material-ui/core/TextField";
-import { Button } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, } from "@material-ui/core";
 import api from 'api';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '100%',
+    },
+  },
+}));
 export default function Login() {
-  const [loginInfo,setLoginInfo]=useState()
+  
+  const classes = useStyles();
+  const [loginInfo,setLoginInfo]=useState();
+  const [index,setIndex]=useState(0);
+  const [value, setValue] = React.useState(1);
+  
+  const handleChange = (event, newValue) => {
+    console.log("hahah",event, newValue);
+    setIndex(newValue);
+  };
   useEffect(()=>{
     api.post("/login",{
       "name": "BiaoChenXuYing",
@@ -22,15 +42,18 @@ export default function Login() {
   const typePassWord = (passWard) => {
     console.log(passWard.target.value);
   };
-  return (
-    <div className=" m-auto mt-6 w-56">
-      Login
+  const Login =()=>{
+    return (<form className={classes.root}  noValidate autoComplete="off">
+    <h1 className=" font-size">Login  </h1>
       <div className="mb-4 mt-3">
         <TextField
           id="outlined-basic"
           label="username"
           variant="outlined"
+          margin="normal"
           onChange={typeUserName}
+          
+          style={{width:"100%"}}
         />
       </div>
       <div>
@@ -41,6 +64,8 @@ export default function Login() {
           autoComplete="current-password"
           variant="outlined"
           onChange={typePassWord}
+         
+          style={{width:"100%"}}
         />
       </div>
       <div className=" mt-3">
@@ -49,12 +74,14 @@ export default function Login() {
           label="email"
           variant="outlined"
           onChange={typeUserName}
+     
+          style={{width:"100%"}}
         />
       </div>
       <div>
         <Button
           variant="contained"
-          className="mt-6"
+          className="mt-6 mb-8"
           onClick={() => {
             console.log(111);
           }}
@@ -62,6 +89,75 @@ export default function Login() {
           submit
         </Button>
       </div>
+      </form>)
+  }
+  const Register =()=>{
+    return (<form className={classes.root}  noValidate autoComplete="off">
+      <h1>Rigster </h1>
+      <div className="mb-4 mt-3">
+        <TextField
+          id="outlined-basic"
+          label="username"
+          variant="outlined"
+          onChange={typeUserName}
+          style={{width:"100%"}}
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="outlined"
+          onChange={typePassWord}
+          style={{width:"100%"}}
+        />
+      </div>
+      <div className=" mt-3">
+      <TextField
+          id="outlined-basic"
+          label="email"
+          variant="outlined"
+          onChange={typeUserName}
+          style={{width:"100%"}}
+        />
+      </div>
+      <div>
+        <Button
+          variant="contained"
+          className="mt-6 mb-6"
+          onClick={() => {
+            console.log(111);
+          }}
+        >
+          Register
+        </Button>
+      </div>
+      </form>)
+  }
+  return (
+    <div className=" m-auto mt-6  ">
+  
+      <div style={{width:"700px",margin:"auto"}}>
+      <Paper square>
+      <Tabs
+        value={value}
+        indicatorColor="primary"
+        textColor="primary"
+        onChange={handleChange}
+        variant="fullWidth"
+        aria-label="disabled tabs example"
+      >
+        <Tab label="LOGIN" />
+        <Tab label="Register" />
+      </Tabs>
+      
+    </Paper>
+    {index===0&&<Login/>}
+      {index===1&&<Register/>}
+      </div>
+      
     </div>
   );
 }
